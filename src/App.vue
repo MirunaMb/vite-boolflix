aad19916b816ef22a14c55b2cb8a207eda8e56d3
MirunaMb <mirunamarcela98@gmail.com>
2023-09-15 15:21:21 +0200

    add configuratio app

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      title: "Hello world"
+    }
+  }
+
+  // components: {
+  //   MyComponent,
+  // },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style lang="scss" >
+@use './styles/general.scss';
+</style>
