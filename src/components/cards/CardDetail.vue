02db60b76f01794c95f70e5ea72ac12db1fbd19d
MirunaMb <mirunamarcela98@gmail.com>
2023-09-23 19:08:52 +0200

    fix styling for cards

--- src/components/cards/CardDetail.vue
+++ src/components/cards/CardDetail.vue
@@ -11,7 +11,7 @@ export default {
   methods: {
     generateImageUrl(posterPath) {
       if (!posterPath) {
-        return 'https://via.placeholder.com/300x450'; 
+        return 'https://via.placeholder.com/300x450';
       }
       const baseUrl = 'https://image.tmdb.org/t/p/';
       const imageSize = 'w342';
@@ -39,22 +39,24 @@ export default {
         <div class="poster-container">
           <img :src="generateImageUrl(cardInfo.posterPath)" :alt="cardInfo.language">
         </div>
-       
+
       </div>
-      
+
       <!-- Contenuto da mostrare al mouseover -->
       <div v-else>
         <div class="info-container">
-        <ul>
-          <li>Nome: {{ cardInfo.name }}</li>
-        <li>Titolo originale: {{ cardInfo.original_title }}</li>
-        <li>Voto: {{ cardInfo.vote }}</li>
-        <li><img :src="'/src/assets/img/' + cardInfo.language + '.png'" :alt="cardInfo.language"></li>
-          <li v-for="n in 5">
-            <font-awesome-icon :icon="getStarClass(n)" />
-          </li>
+          <ul>
+            <li>Nome: {{ cardInfo.name }}</li>
+            <li>Titolo originale: {{ cardInfo.original_title }}</li>
+            <li>Voto: {{ cardInfo.vote }}</li>
+            <li><img :src="'/src/assets/img/' + cardInfo.language + '.png'" :alt="cardInfo.language"></li>
+            <li>
+              <div class="stars">
+                <font-awesome-icon v-for="n in 5" :key="n" :icon="getStarClass(n)" />
+              </div>
+            </li>
 
-        </ul>
+          </ul>
         </div>
       </div>
     </div>
@@ -62,33 +64,41 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-.card{
+.card {
   width: 300px;
   height: 450px;
 }
+
 .card-body {
   transition: transform 0.3s ease;
   transform-style: preserve-3d;
-  width: 100%; 
-  height: 100%; 
-  display: flex;
-  justify-content: center; 
-  align-items: center; 
-  position: relative; 
-}
-.poster-container, .info-container {
-  width: 100%; 
+  width: 100%;
   height: 100%;
-  position: absolute; 
+  display: flex;
+  justify-content: center;
+  padding-top: 150px;
+}
+
+.poster-container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
   top: 0;
   left: 0;
-  display: flex; 
-  justify-content: center; 
-  align-items: center; 
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-.poster-container img, .info-container div {
-  max-width: 100%; 
-  max-height: 100%; 
+
+.poster-container img,
+.info-container div {
+  max-width: 100%;
+  max-height: 100%;
+}
+
+.info-container {
+  display: flex;
+
 }
 
 .hovered {
@@ -108,12 +118,10 @@ export default {
   list-style: none;
 }
 
-.hovered li{
+.hovered li {
   margin: 0;
   padding: 0;
   list-style: none;
   display: flex;
-  justify-content: flex-end;
-  
 }
 </style>
--- src/components/cards/TvSeriesDetail.vue
+++ src/components/cards/TvSeriesDetail.vue
@@ -1,14 +1,19 @@
 <script>
 
 export default {
- props:{
- tvSeriesInfo:Object,
- },
- methods: {
+  data() {
+    return {
+      isHovered: false,
+    };
+  },
+  props: {
+    tvSeriesInfo: Object,
+  },
+  methods: {
     generateImageUrl(posterPath) {
       if (!posterPath) {
         // in caso in cui non ci sia un percorso per la copertina 
-        return 'https://via.placeholder.com/300x450'; 
+        return 'https://via.placeholder.com/300x450';
       }
 
       // URL base delle immagini
@@ -20,24 +25,107 @@ export default {
 
       return fullImageUrl;
     },
-    getStarClass(n){
+    getStarClass(n) {
       return n <= this.tvSeriesInfo.vote ? 'fa-solid fa-star' : 'fa-regular fa-star';
-    }
+    },
+    showDetails() {
+      this.isHovered = true;
+    },
+    hideDetails() {
+      this.isHovered = false;
+    },
   },
 };
 </script>
 
 <template>
-<div class="card">
-    <div class="card-body">
-      <li>{{ tvSeriesInfo.name  }}</li>
-     <li> {{ tvSeriesInfo.original_name}}</li>
-      <li><img :src="'/src/assets/img/' + tvSeriesInfo.language + '.png'" :alt="tvSeriesInfo.language"></li>
-     <li>{{ tvSeriesInfo.vote }}</li>
-     <font-awesome-icon :icon="getStarClass(n)" v-for="n in 5"/>
-     <li> <img :src="generateImageUrl(tvSeriesInfo.posterPath)" :alt="tvSeriesInfo.language"></li>
+  <div class="card" @mouseover="showDetails" @mouseout="hideDetails">
+    <div class="card-body" :class="{ 'hovered': isHovered }">
+      <!-- Contenuto da mostrare al mouseout -->
+      <div v-if="!isHovered">
+        <div class="poster-container">
+          <img :src="generateImageUrl(tvSeriesInfo.posterPath)" :alt="tvSeriesInfo.language">
+        </div>
+
+      </div>
+
+      <!-- Contenuto da mostrare al mouseover -->
+      <div v-else>
+        <div class="info-container">
+          <ul>
+            <li>Nome: {{ tvSeriesInfo.name }}</li>
+            <li>Titolo originale: {{ tvSeriesInfo.original_name }}</li>
+            <li>Voto: {{ tvSeriesInfo.vote }}</li>
+            <li><img :src="'/src/assets/img/' + tvSeriesInfo.language + '.png'" :alt="tvSeriesInfo.language"></li>
+            <li v-for="n in 5">
+              <font-awesome-icon :icon="getStarClass(n)" />
+            </li>
+
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card {
+  width: 300px;
+  height: 450px;
+}
+
+.card-body {
+  transition: transform 0.3s ease;
+  transform-style: preserve-3d;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+
+.poster-container,
+.info-container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.poster-container img,
+.info-container div {
+  max-width: 100%;
+  max-height: 100%;
+}
+
+.hovered {
+  transform: rotateY(360deg);
+  background-color: #fff;
+  transform-style: preserve-3d;
+}
+
+.hovered div {
+  transform: rotateY(180deg);
+  transform-style: preserve-3d;
+}
+
+.hovered ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+.hovered li {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  justify-content: flex-end;
+
+}
+</style>
